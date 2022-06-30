f6b75ccd9d248ab623bbf879ec3772f10d921275
Sindre Stokke <sindre.stokke@nmbu.no>
2022-06-30 23:17:11 +0200

    La til gatekjøp, la til flysammenligning og fikset bug med sjansekortoppslaget for sjansekort-df

--- simulering.py
+++ simulering.py
@@ -5,6 +5,7 @@ import random
 import statistics
 import time
 
+
 class Spiller():
     def __init__(self, spiller_nr):
         self.nr = spiller_nr
@@ -17,7 +18,8 @@ class Spiller():
             "terningkast": [0],
             "runde": [self.antall_runder],
             "antall_kast": [0],
-            "vandret_avstand": [0]
+            "vandret_avstand": [0],
+            "kjøpt_gate": [0]
             }
     
     def kast_terninger(self):
@@ -30,6 +32,15 @@ class Spiller():
         self.terningkast = terningkast
     
     def flytt(self, ankommet_felt, terningkast):
+        # Kjøp gate
+        felt_b4_flytt = self.flyttehistorikk["ankommet_felt"][-1]
+        buy_gate = 0
+        if felt_b4_flytt in buyable_gater:
+            buy_gate = felt_b4_flytt
+            buyable_gater.remove(buy_gate)
+        self.flyttehistorikk["kjøpt_gate"].append(buy_gate)
+        
+        # Registrer flytt til flyttehistorikk
         self.flyttehistorikk["flytt"].append(self.flyttehistorikk["flytt"][-1]+1)
         self.flyttehistorikk["forrige_felt"].append(self.flyttehistorikk["ankommet_felt"][-1])
         self.flyttehistorikk["ankommet_felt"].append(ankommet_felt)
@@ -42,7 +53,7 @@ class Spiller():
         else:
             self.flyttehistorikk["antall_kast"].append(self.flyttehistorikk["antall_kast"][-1]+1)
 
-    def flytt_fra_terningkast(self, antall_felter):
+    def flytt_fra_terningkast(self, antall_felter, buyable_gater):
         felt_b4_flytt = self.flyttehistorikk["ankommet_felt"][-1]
         feltplussterningkast = felt_b4_flytt + self.terningkast
         antall_felter = antall_felter
@@ -55,12 +66,12 @@ class Spiller():
         
         self.flytt(nytt_felt, self.terningkast)
     
-    def dobbelt_kast(self, antall_felter, sjansekort, spillere):
+    def dobbelt_kast(self, antall_felter, sjansekort, spillere, buyable_gater):
         if self.terningkast == 12:
             self.kast_terninger()
-            self.flytt_fra_terningkast(antall_felter)
+            self.flytt_fra_terningkast(antall_felter, buyable_gater)
             self.fengsel()
-            self.sjansekort(sjansekort)
+            self.sjansekort(sjansekort, buyable_gater)
         
     def fengsel(self):
         felt_b4_flytt = self.flyttehistorikk["ankommet_felt"][-1]
@@ -68,13 +79,13 @@ class Spiller():
         if felt_b4_flytt == 31:
             self.flytt(11, 0)
     
-    def sjansekort(self, sjansekort):
+    def sjansekort(self, sjansekort, buyable_gater):
         felt_b4_flytt = self.flyttehistorikk["ankommet_felt"][-1]
-        if felt_b4_flytt in [3, 8, 18, 23, 34, 37]:
+        if felt_b4_flytt in sjansekort["relokaliserende_kort"]:
             """
             Sjekker om man har ankommet et sjansekort og utfører isåfall sjansekortets funksjon.
             """
-            trukket_sjansekort = sjansekort[0]
+            trukket_sjansekort = sjansekort["bunke"][0]
             
             if trukket_sjansekort == 19:
                 nytt_felt = 31
@@ -106,8 +117,8 @@ class Spiller():
             if trukket_sjansekort in [19, 20, 21, 22, 23, 24, 25, 26, 27]:
                 self.flytt(nytt_felt, 0)
             
-            sjansekort.pop(0)
-            sjansekort.append(trukket_sjansekort)
+            sjansekort["bunke"].pop(0)
+            sjansekort["bunke"].append(trukket_sjansekort)
 
 
 def spilloppsett():
@@ -117,12 +128,17 @@ def spilloppsett():
     antall_felter = 40
     antall_spillere = 4
     antall_sjansekort = 35
+    buyable_gater = [2, 4, 6, 7, 9, 10, 12, 13, 14, 15, 16, 17, 19, 20, 22,\
+                     24, 25, 26, 27, 28, 29, 30, 32, 33, 35, 36, 38, 40]
     
-    sjansekort = [n for n in range(1,antall_sjansekort+1)] # Genererer sjansekort
-    random.shuffle(sjansekort) # Stokker sjansekortene
+    sjansekort = {
+        "bunke": [n for n in range(1,antall_sjansekort+1)], # Genererer sjansekort
+        "relokaliserende_kort": [3, 8, 18, 23, 34, 37]
+        }
+    random.shuffle(sjansekort["bunke"]) # Stokker sjansekortene
     spillere = [Spiller(spiller_nr+1) for spiller_nr in range(antall_spillere)]
     
-    return antall_felter, sjansekort, spillere
+    return antall_felter, sjansekort, spillere, buyable_gater
 
 
 def spill_loop(antall_turer, antall_felter, sjansekort, spillere):
@@ -130,17 +146,17 @@ def spill_loop(antall_turer, antall_felter, sjansekort, spillere):
     for tur in range(antall_turer):
         for spiller in spillere:
             spiller.kast_terninger()
-            spiller.flytt_fra_terningkast(antall_felter)
+            spiller.flytt_fra_terningkast(antall_felter, buyable_gater)
             spiller.fengsel()
-            spiller.sjansekort(sjansekort)
-            spiller.dobbelt_kast(antall_felter, sjansekort, spillere)
+            spiller.sjansekort(sjansekort, buyable_gater)
+            spiller.dobbelt_kast(antall_felter, sjansekort, spillere, buyable_gater)
     t1 = time.time()
     return t1-t0
 
 
-def lagre_spillerdata_til_df(spillere):
-    # Merger spillerdataene til en dictionary
-    spillerdata = {
+def lagre_flyttedata_til_df(spillere):
+    # Merger flyttedataene til en dictionary
+    flyttedata = {
         "spiller": [],
         "flytt": [],
         "forrige_felt": [],
@@ -148,26 +164,32 @@ def lagre_spillerdata_til_df(spillere):
         "terningkast": [],
         "runde": [],
         "antall_kast": [],
-        "vandret_avstand": []
+        "vandret_avstand": [],
+        "kjøpt_gate": []
         }
     for spiller in spillere:
         for flytt in spiller.flyttehistorikk["flytt"]:
-            spillerdata["spiller"].append(spiller.nr)
-            spillerdata["flytt"].append(spiller.flyttehistorikk["flytt"][flytt])
-            spillerdata["forrige_felt"].append(spiller.flyttehistorikk["forrige_felt"][flytt])
-            spillerdata["ankommet_felt"].append(spiller.flyttehistorikk["ankommet_felt"][flytt])
-            spillerdata["terningkast"].append(spiller.flyttehistorikk["terningkast"][flytt])
-            spillerdata["runde"].append(spiller.flyttehistorikk["runde"][flytt])
-            spillerdata["antall_kast"].append(spiller.flyttehistorikk["antall_kast"][flytt])
-            spillerdata["vandret_avstand"].append(spiller.flyttehistorikk["vandret_avstand"][flytt])
+            flyttedata["spiller"].append(spiller.nr)
+            flyttedata["flytt"].append(spiller.flyttehistorikk["flytt"][flytt])
+            flyttedata["forrige_felt"].append(spiller.flyttehistorikk["forrige_felt"][flytt])
+            flyttedata["ankommet_felt"].append(spiller.flyttehistorikk["ankommet_felt"][flytt])
+            flyttedata["terningkast"].append(spiller.flyttehistorikk["terningkast"][flytt])
+            flyttedata["runde"].append(spiller.flyttehistorikk["runde"][flytt])
+            flyttedata["antall_kast"].append(spiller.flyttehistorikk["antall_kast"][flytt])
+            flyttedata["vandret_avstand"].append(spiller.flyttehistorikk["vandret_avstand"][flytt])
+            flyttedata["kjøpt_gate"].append(spiller.flyttehistorikk["kjøpt_gate"][flytt])
     
-    # Lagrer spillerdataene til DataFrame
-    df = pd.DataFrame(spillerdata)
+    # Lagrer flyttedataene til DataFrame
+    df = pd.DataFrame(flyttedata)# Gatekjøp
+    df_betalingspliktige = df[ ~(df["forrige_felt"] == 21) ]
+    df_gate_buys = df[ ~(df["kjøpt_gate"] == 0) ]
+    df_betalingspliktige_flyankomster =\
+        df_betalingspliktige[ df_betalingspliktige["ankommet_felt"].isin([13, 29]) ]
     
-    return df
+    return df, df_betalingspliktige, df_gate_buys, df_betalingspliktige_flyankomster
 
 
-def statistikk(antall_felter, df, spillere):
+def statistikk(antall_felter, spillere, df, df_gate_buys, df_betalingspliktige_flyankomster):
     # Teller opp antall terningkast
     statistikk_terningkast = []
     for terningkast in [n for n in range(0,13)]:
@@ -188,7 +210,10 @@ def statistikk(antall_felter, df, spillere):
         "b_betalingspliktige": np.zeros(antall_felter), # antall betalingspliktige besøk
         "bpr_totalt": [], # antall besøk totalt per runde
         "bpr_betalingspliktige": [], # antall betalingspliktige besøk per runde
-        "bpr_besøk_fra_sjansekort": [] # antall besøk fra sjansekort per runde
+        "bpr_besøk_fra_sjansekort": [], # antall besøk fra sjansekort per runde
+        "sum_terningøyne_flyselskap": {"Felt 13": 0, "Felt 29": 0,
+                                       "Felt 13 / Felt 29": 0,
+                                       "Felt 29 / Felt 13": 0}
         }
     
     for felt in range(1,antall_felter+1):
@@ -198,7 +223,7 @@ def statistikk(antall_felter, df, spillere):
             df[ (df["ankommet_felt"] == felt) & (df["forrige_felt"] == 21) ].shape[0]
         statistikk_felter["b_fra_sjansekort"][felt-1] =\
             df[ (df["ankommet_felt"] == felt) &\
-               (df["forrige_felt"].isin([19, 20, 21, 22, 23, 24, 25, 26, 27])) &\
+               (df["forrige_felt"].isin(sjansekort["relokaliserende_kort"])) &\
                    (df["terningkast"] == 0) ].shape[0]
     
     statistikk_felter["b_betalingspliktige"] =\
@@ -213,24 +238,35 @@ def statistikk(antall_felter, df, spillere):
     statistikk_felter["bpr_besøk_fra_sjansekort"] =\
         statistikk_felter["b_fra_sjansekort"] / sum(statistikk_spillere["antall_runder"])
     
+    # Flyankomster
+    statistikk_felter["sum_terningøyne_flyselskap"]["Felt 13"] =\
+        sum(df_betalingspliktige_flyankomster[ df_betalingspliktige_flyankomster["ankommet_felt"] == 13 ]["terningkast"])
+    statistikk_felter["sum_terningøyne_flyselskap"]["Felt 29"] =\
+        sum(df_betalingspliktige_flyankomster[ df_betalingspliktige_flyankomster["ankommet_felt"] == 29 ]["terningkast"])
+    statistikk_felter["sum_terningøyne_flyselskap"]["Felt 13 / Felt 29"] =\
+        statistikk_felter["sum_terningøyne_flyselskap"]["Felt 13"] / statistikk_felter["sum_terningøyne_flyselskap"]["Felt 29"]
+    statistikk_felter["sum_terningøyne_flyselskap"]["Felt 29 / Felt 13"] =\
+        statistikk_felter["sum_terningøyne_flyselskap"]["Felt 29"] / statistikk_felter["sum_terningøyne_flyselskap"]["Felt 13"]
+    
     return statistikk_spillere, statistikk_felter, statistikk_terningkast
 
 
 if __name__ == "__main__":
     # Setter opp spillet
-    antall_felter, sjansekort, spillere = spilloppsett()
+    antall_felter, sjansekort, spillere, buyable_gater = spilloppsett()
     
     # Gjennomfører turene
     antall_turer = int((5.6)*10**4)
     tidsbruk = spill_loop(antall_turer, antall_felter, sjansekort, spillere)
     
     # Statistikk
-    df = lagre_spillerdata_til_df(spillere)
-    statistikk_spillere, statistikk_felter, statistikk_terningkast = statistikk(antall_felter, df, spillere)
+    df, df_betalingspliktige, df_gate_buys, df_betalingspliktige_flyankomster = lagre_flyttedata_til_df(spillere)
+    statistikk_spillere, statistikk_felter, statistikk_terningkast =\
+        statistikk(antall_felter, spillere, df, df_gate_buys, df_betalingspliktige_flyankomster)
     
     # Printing
     print("Turer: " + str(antall_turer))
-    #print("Runder: " + str(statistikk["runder_per_spiller"]))
-    #print("Kast per runde: " + str(antall_turer/statistikk["runder_per_spiller"]))
-    print("Tid brukt: " + str(round(tidsbruk, 2)) + " sekunder")
+    print("Runder: " + str(sum(statistikk_spillere["antall_runder"])))
+    print("Flytt per runde: " + str(antall_turer/(sum(statistikk_spillere["antall_runder"])/len(statistikk_spillere["antall_runder"]))))
+    print("Simuleringstid: " + str(round(tidsbruk, 2)) + " sekunder")
     
